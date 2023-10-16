b28f7c8eee22e30dbfabe444528a4a433036b902
XRequiem1 <144215937+XRequiem1@users.noreply.github.com>
2023-10-16 11:19:44 +0300

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,63 @@
+#1
+#ref_list = [['name1', 'name2'], ['surname', 'surname2']]
+
+#def list_reorder(ref_list):
+#    a_list = ['name1']
+#    a_list2 = ['name2']
+#    b_list = ['surname']
+#    b_list2 = ['surname2']
+
+
+#print(list_reorder(ref_list))
+
+#2
+#num = [1, 3, 4, 2, 1, 4, 1, 2, 3, 3, 2, 1, 3, 4, 2, 2]
+#    def del_rep(num):
+#    num.sort()
+#    end = num
+#    print(res)
+
+#3
+# вводимые значения
+#n = int(input('число'))
+#n2 = int(input('число'))
+#n3 = int(input('число'))
+#n4 = int(input('число'))
+# список со значениями
+#nums = [n, n2, n3, n4]
+# лимит
+#limit = int(input('лимит'))
+# функция с возвращением ограниченного максимума
+#def lim_max(nums, limit):
+#    if n < limit:
+#        return n
+#    elif n2 < limit:
+#        return n2
+#    elif n3 < limit:
+#        return n3
+#    elif n4 < limit:
+#        return n4
+    # выполнить функцию
+#print(lim_max(nums, limit))
+
+
+#4
+# любое число
+#temp = int(input())
+# функция с возвращением значения к переменной
+#def temp_cat(temp):
+#    if temp < -20:
+#        return '0 #Холодно'
+#    elif 0 > temp > -20:
+#        return'1 #Прохладно'
+#    elif 0 < temp < 15:
+#        return'2 #Зябко'
+#    elif 25 > temp > 15:
+#        return'3 #Тепло'
+#    elif temp > 20:
+#        return '4 #Жарко'
+# написать возвращённое значение
+#print(temp_cat(temp))
+
+
+
